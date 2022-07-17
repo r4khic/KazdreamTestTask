906a30795677c4feb03d7a01b5ad8757c0862c1b
r4khic <r4khic@gmail.com>
2022-07-17 11:38:33 +0600

    added api

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask
+import json
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/smartphones', methods=['GET'])
+def search():
+    price = request.args.get('price', type=int)
+    f = open('satukzParser/smartphones.json', 'r')
+    datas = json.load(f)
+    for data in datas:
+        if data['price']:
+            if price == int(data['price']):
+                return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
