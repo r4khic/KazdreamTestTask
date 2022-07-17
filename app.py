87d224db158026c6b278fa3d63535aa9143ab151
r4khic <r4khic@gmail.com>
2022-07-17 12:08:12 +0600

    finally project ended

--- app.py
+++ app.py
@@ -7,13 +7,15 @@ app.config['JSON_SORT_KEYS'] = False
 
 @app.route('/smartphones', methods=['GET'])
 def search():
+    sorted_data = []
     price = request.args.get('price', type=int)
     f = open('satukzParser/smartphones.json', 'r')
     datas = json.load(f)
     for data in datas:
         if data['price']:
             if price == int(data['price']):
-                return jsonify(data)
+                sorted_data.append(data)
+    return jsonify(sorted_data)
 
 
 if __name__ == '__main__':
